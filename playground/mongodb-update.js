bed645edbd4d68699287d015b977dd2fcdabdd88
sai abhinav <saiabhinavvasepalli@gmail.com>
2018-11-21 20:29:51 +0530

    Added update script

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -20,16 +20,29 @@ MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
     //     console.log(result);
     // });
 
-    db.collection('Users').findOneAndUpdate({
-        _id: new ObjectID('5bf5064198073e21b07ba77a')
-    }, {
-        $set: { name: 'Abhinav' },
-        $inc: { age: 5 }
-    }, {
-        returnOriginal: false
-    }).then((result) => {
-        console.log(result);
-    });
+    // db.collection('Users').findOneAndUpdate({
+    //     _id: new ObjectID('5bf5064198073e21b07ba77a')
+    // }, {
+    //     $set: { name: 'Abhinav' },
+    //     $inc: { age: 5 }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
 
-    client.close();
+    var colName = 'Todos';
+
+    db.collection(colName).find({ text: "Eat lunch" }).toArray().then((docs) => {
+        console.log(`Task: ${docs[0].text}.\nStatus: ${docs[0].completed}.\nChanging...`);
+        var newStatus = !docs[0].completed;
+        db.collection(colName).findOneAndUpdate({
+            _id: new ObjectID(docs[0]._id)
+        }, {
+            $set: { completed: newStatus }
+        }, { returnOriginal: false }).then((result) => {
+            console.log(`Task: ${result.value.text}.\nStatus: ${result.value.completed}.`);
+            client.close();
+        });
+    });    
 });
