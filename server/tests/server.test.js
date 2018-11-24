5fc26ee6be73961c4f9dd0913fbf380125e80ada
sai abhinav <saiabhinavvasepalli@gmail.com>
2018-11-24 12:54:40 +0530

    Updated all the test cases with latest expect functions

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -135,7 +135,7 @@ describe('DELETE /todos/:id', () => {
                 }
 
                 Todo.findById(hexId).then((todo) => {
-                    expect(todo).toExist();
+                    expect(todo).toBeTruthy();
                     done();
                 }).catch((e) => done(e));
             });
@@ -172,7 +172,8 @@ describe('PATCH /todos/:id', () => {
             .expect(200)
             .expect((res) => {
                 expect(res.body.todo.text).toBe(text);
-                expect(res.body.todo.completed).toBeTruthy();                
+                expect(res.body.todo.completed).toBeTruthy();  
+                expect(typeof res.body.todo.completedAt).toBe('number');              
             })
             .end(done);
     });
@@ -230,7 +231,7 @@ describe('POST /users', () => {
                 }
                 User.findOne({email}).then((user) => {
                     expect(user).toBeTruthy();
-                    expect(user.password).toNotBe(password);
+                    expect(user.password).not.toBe(password);
                     done();
                 }).catch((e) => done(e));
             });
@@ -300,7 +301,7 @@ describe('POST /users/login', () => {
                     return done(err);
                 }
                 User.findById(users[1]._id).then((user) => {
-                    expect(user.tokens[1]).toInclude({
+                    expect(user.toObject().tokens[1]).toMatchObject({
                         access: 'auth',
                         token: res.headers['x-auth']
                     });                       
@@ -318,7 +319,7 @@ describe('POST /users/login', () => {
             })
             .expect(400)            
             .expect((res) => {
-                expect(res.headers['x-auth']).toNotExist();
+                expect(res.headers['x-auth']).toBeFalsy();
             })
             .end((err, res) => {
                 if (err) {
